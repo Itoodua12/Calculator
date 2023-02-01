ce7913201ce473e60b2f25556a07ad1ed8c41b36
itoodua12 <itoodua@gmail.com>
2023-02-01 18:08:47 +0000

    to be continued

--- calculatorApp/script.js
+++ calculatorApp/script.js
@@ -8,6 +8,7 @@ function printSymbol(e) {
 
 let result = document.getElementById('result');
 
+const num0 = document.getElementById('num0');
 const num1 = document.getElementById('num1');
 const num2 = document.getElementById('num2');
 const num3 = document.getElementById('num3');
@@ -17,8 +18,14 @@ const num6 = document.getElementById('num6');
 const num7 = document.getElementById('num7');
 const num8 = document.getElementById('num8');
 const num9 = document.getElementById('num9');
+const sub = document.getElementById('sub');
+const plus = document.getElementById('plus');
+const mult = document.getElementById('mult');
+const div = document.getElementById('div');
+const decimal = document.getElementById('decimal');
+const clear = document.getElementById('clear');
 
-
+num0.addEventListener('click', printSymbol);
 num1.addEventListener('click', printSymbol);
 num2.addEventListener('click', printSymbol);
 num3.addEventListener('click', printSymbol);
@@ -28,6 +35,19 @@ num6.addEventListener('click', printSymbol);
 num7.addEventListener('click', printSymbol);
 num8.addEventListener('click', printSymbol);
 num9.addEventListener('click', printSymbol);
+sub.addEventListener('click', printSymbol);
+plus.addEventListener('click', printSymbol);
+mult.addEventListener('click', printSymbol);
+div.addEventListener('click', printSymbol);
+decimal.addEventListener('click', printSymbol);
+clear.addEventListener('click', () => document.getElementById('result')
+                                .textContent = 0);
+
+
+
+
+
+
 
 
 
