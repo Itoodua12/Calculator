e024c15ee602831ab0055d59ffa976d50eafc901
itoodua12 <itoodua@gmail.com>
2023-02-02 15:49:37 +0000

    removed redundant code

--- calculatorApp/script.js
+++ calculatorApp/script.js
@@ -19,26 +19,6 @@ function printSymbol(e) {
 
 }
 
-
-const result = document.getElementById('result');
-const num0 = document.getElementById('num0');
-const num1 = document.getElementById('num1');
-const num2 = document.getElementById('num2');
-const num3 = document.getElementById('num3');
-const num4 = document.getElementById('num4');
-const num5 = document.getElementById('num5');
-const num6 = document.getElementById('num6');
-const num7 = document.getElementById('num7');
-const num8 = document.getElementById('num8');
-const num9 = document.getElementById('num9');
-const sub = document.getElementById('sub');
-const plus = document.getElementById('plus');
-const mult = document.getElementById('mult');
-const div = document.getElementById('div');
-const decimal = document.getElementById('decimal');
-const leftp = document.getElementById('leftp');
-const rightp = document.getElementById('rightp');
-
 const clear = document.getElementById('clear');
 const equal = document.getElementById('equal');
 
