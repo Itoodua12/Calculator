36845478cafe82608e1cea9a64eb857b90ed015e
itoodua12 <itoodua@gmail.com>
2023-02-02 16:31:31 +0000

    edge case when want to type deciam which starts with zero

--- calculatorApp/script.js
+++ calculatorApp/script.js
@@ -1,18 +1,13 @@
 
-
-// function printSymbol(e) {
-//     const result = document.getElementById('result');
-//     const obj = e.target;
-
-//     result.value += obj.textContent;
-
-// }
-
 function printSymbol(e) {
     const result = document.getElementById('result');
     const obj = e.target;
 
-    if(result.value == 0) 
+    if(result.value == 0 && obj.textContent == '.') {
+        result.value += obj.textContent;
+    } else if(result.value == '0.') {
+        result.value += obj.textContent;
+    } else if(result.value == 0) 
         result.value = obj.textContent;
     else
         result.value += obj.textContent;
@@ -24,8 +19,6 @@ const equal = document.getElementById('equal');
 
 const symbols = document.getElementsByClassName('symbol');
 
-// const symbols = [num0,num1,num2,num3,num4,num5,num6,num7,num8,num9,
-//             plus,mult,sub,div,decimal,rightp,leftp];
 
 for(let i = 0; i< symbols.length; i++) {
     symbols[i].addEventListener('click', printSymbol);
